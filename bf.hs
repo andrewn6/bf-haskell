75b2ca9d43957937f3b714343e70ca10ba337b99
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-09-06 12:13:30 -0400

    finish adding rest of valid commands, and execute them

--- bf.hs
+++ bf.hs
@@ -42,4 +42,30 @@ goLeft = S.modify zipLeft
 goRight :: Program ()
 goRight = S.modify zipRight
 
-execute ::
+execute :: (ListZipper Char) -> Program ()
+execute ([],_) = return ()
+execute xs@(x:_,_) = do
+    (y:ys,yss) <- S.get
+    let next = execute $ zipRight xs in case x of
+        '>' -> goRight >> next
+        '<' -> goLeft >> next
+        '+' -> increment >> next
+        '-' -> decrement >> next
+        '.' -> (T.liftIO $ putChar $ chr y) >> next
+        ',' -> do { z <- T.liftIO $ getChar; S.put ((ord z):ys,yss)  } >> next
+        '[' -> if y /= 0 then next else execute $ helper 0 ']' '[' $ tail $ iterate zipRight xs
+        ']' -> if y == 0 then next else execute $ helper 0 '[' ']' $ tail $ iterate zipLeft xs
+
+isBrace b = (b ==) . head . fst
+
+helper n o c (x:xs)
+    | (n == 0) && isBrace o x = x
+    | isBrace o x = helper (n - 1) o c xs
+    | isBrace c x = helper (n + 1) o c xs
+    | otherwise = helper n o c xs
+
+
+main = do
+    program <- fmap interpret getLine
+    S.evalStateT (execute (program,[])) (repeat 0,[])
+
